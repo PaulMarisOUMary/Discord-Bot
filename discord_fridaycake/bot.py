5e7b2414dea11728d847a1cd4d6113182e9ca829
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-01-15 20:48:13 +0100

    add ?rename & fix bugs

--- discord_fridaycake/bot.py
+++ discord_fridaycake/bot.py
@@ -7,13 +7,21 @@ from discord.ext import commands
 from discord.utils import get
 from datetime import datetime
 
-text_channels, text_roles = [], []
+def get_created_roles(cont):
+	wrong_roles = []
+	for role in cont.guild.roles:
+		perm = cont.channel.overwrites_for(role)
+		if perm.send_messages:
+			wrong_roles.append(role)
+
+	return wrong_roles
 
 async def loop_if_connected():
-	GUILD, CATEGORY, MAIN_CHANNEL = "Algosup Alpha", "Voice rooms", "General"
+	GUILD, MAIN_CHANNEL = "Serveur de test", "General"
 	await bot.wait_until_ready()
 	guild = discord.utils.get(bot.guilds, name=GUILD)
-	category, channel = discord.utils.get(guild.categories, name=CATEGORY), discord.utils.get(guild.channels, name=MAIN_CHANNEL)
+	channel = discord.utils.get(guild.channels, name=MAIN_CHANNEL)
+	category = discord.utils.get(guild.categories, id=channel.category_id)
 	channels = [channel]
 
 	def channelInfos():
@@ -53,12 +61,12 @@ async def loop_if_connected():
 
 async def loop_change_status():
 	await bot.wait_until_ready()
-	status_message, sm = ["?help","in lockdown.."], 0
+	status_message, reset = ["?help","in lockdown..", "new ?+"], 0
 	while not bot.is_closed():
-		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=status_message[sm], url='https://www.twitch.tv/warriormachine_'), status=discord.Status.dnd, afk=False)
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=status_message[reset], url='https://www.twitch.tv/warriormachine_'), status=discord.Status.dnd, afk=False)
 		await asyncio.sleep(10)
-		if sm >= len(status_message)-1: sm = 0
-		else: sm+=1
+		if reset >= len(status_message)-1: reset = 0
+		else: reset += 1
 
 class Usefull(commands.Cog):
 	def __init__(self, bot):
@@ -70,66 +78,72 @@ class Usefull(commands.Cog):
 		embed.set_footer(text="Requested by : "+str(ctx.message.author)+" à "+str(time.strftime('%H:%M:%S')), icon_url=ctx.message.author.avatar_url)
 		await ctx.send(embed=embed)
 
-	@commands.command(name='addprivate', aliases=['+', '>'], require_var_positional=True)
-	@commands.cooldown(1, 50, commands.BucketType.user)
-	async def private_channels(self, ctx, *guys : discord.Member):
-		text_category, users, mentions, you_in, down_role = discord.utils.get(ctx.guild.categories, name="Text rooms"), [], "", False, discord.utils.get(ctx.guild.roles, name="🎓Élève")
-		
+	@commands.command(name='addprivate', aliases=['create', 'add', '+', '>'], require_var_positional=True)
+	@commands.cooldown(1, 10, commands.BucketType.user)
+	async def create_private_channel(self, ctx, *guys : discord.Member):
+		users, mentions, down_role = [ctx.message.author], "", discord.utils.get(ctx.guild.roles, name="🎓Élève")
+
 		for g in guys:
 			if g.bot: raise commands.CommandError("bot.notAllowed")
 			else: users.append(g)
-			if g.id == ctx.message.author.id: you_in = True
-		if not you_in: raise commands.CommandError("author.requestedIn")
-		elif len(guys) <= 1: raise commands.CommandError("author.isAlone")
+		users = list(set(users))
+		if len(users) <= 1: raise commands.CommandError("author.isAlone")
 
-		role = await ctx.guild.create_role(name="tr_"+str(len(text_roles)+1))
-		a = await ctx.guild.create_text_channel(name="team_text_"+str(len(text_channels)+1), category=text_category, sync_permissions=False)
-		await a.set_permissions(ctx.guild.default_role, send_messages=False, view_channel=False)
-		await a.set_permissions(role, send_messages=True, view_channel=True)
-		await a.set_permissions(down_role, send_messages=False, view_channel=False)
-		text_channels.append(a)
-		text_roles.append(role)
+		role = await ctx.guild.create_role(name="team")
+		team_channel = await ctx.guild.create_text_channel(name="_team_text", category=discord.utils.get(ctx.guild.categories, id=ctx.channel.category_id), sync_permissions=False)
+		await team_channel.set_permissions(role, send_messages=True, view_channel=True)
+		await team_channel.set_permissions(down_role, send_messages=False, view_channel=False)
 
-		for m in users:
-			await m.add_roles(role)
-			mentions += " "+m.mention
-		
-		await a.send("The text_team_"+str(len(text_channels))+" was created by "+str(ctx.message.author.mention)+".")
-		await a.send(mentions)
+		for user in users:
+			await user.add_roles(role)
+			mentions += " "+user.mention
+
+		await team_channel.send(str(team_channel.mention)+" was created by "+str(ctx.message.author.mention)+".")
+		await team_channel.send(mentions)
+		await ctx.message.add_reaction(emoji='✅')
 	
-	@commands.command(name='delprivate', aliases=['-', '<'])
-	async def delete_private_channels(self, ctx):
-		channel_name, is_error, deleted = ctx.channel.name, True, False
-		for i, ch in enumerate(text_channels):
-			if ch.name == channel_name:
-				await ch.delete()
-				del text_channels[i]
-				await text_roles[i].delete()
-				del text_roles[i]
-				is_error, deleted = False, True
+	@commands.command(name='delprivate', aliases=['delete', 'del', '-', '<'])
+	async def delete_private_channel(self, ctx):
+		channel, roles = ctx.channel, get_created_roles(ctx)
+		if '_' in channel.name and roles:
+			await roles[0].delete()
+			await channel.delete()
+		else:
+			await ctx.send("Error, you can't delete a non-team channel.")
+			await ctx.message.add_reaction(emoji='❌')
 
-		if not deleted and channel_name[0:10] == "team_text_":
-			await ctx.channel.delete()
-			is_error = False
-		
-		if is_error:
-			await ctx.send("Error, you can't delete this channel")
+	@commands.command(name='renprivate', aliases=['rename', 'ren', 'r', '_'], require_var_positional=True)
+	async def rename_private_channel(self, ctx, custom_name : str):
+		channel, roles = ctx.channel, get_created_roles(ctx)
+		if '_' in channel.name and roles and custom_name:
+			await channel.edit(name='_'+custom_name)
+			await ctx.message.add_reaction(emoji='✅')
+		else:
+			await ctx.send("Error, you can't rename a non-team channel.")
+			await ctx.message.add_reaction(emoji='❌')
 
 	### ERRORS ###
-	@private_channels.error
-	async def pv_command_error(self, ctx, error):
+	@create_private_channel.error
+	async def private_channel_error(self, ctx, error):
 		if isinstance(error, commands.MissingRequiredArgument):
 			await ctx.send('Please specify users which you want to add : `?pv @user1 @user2`')
 		elif isinstance(error, commands.CommandOnCooldown):
 			await ctx.send('Command is on cooldown, wait `'+str(error)[-6:-4]+'s` !')
 		elif str(error) == 'bot.notAllowed':
 			await ctx.send("Error, you can't invite bots in your team !")
-		elif str(error) == 'author.requestedIn':
-			await ctx.send("Error, you are not in the group !")
 		elif str(error) == 'author.isAlone':
 			await ctx.send("Error, you can't create a team alone.")
 		else:
 			await ctx.send('Error, check the arguments provided')
+		await ctx.message.add_reaction(emoji='❌')
+
+	@rename_private_channel.error
+	async def rename_channel_error(self, ctx, error):
+		if isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send('Please specify the custom title for the channel : `?_ {name_whitout_space}`')
+		else:
+			await ctx.send('Error, check the arguments provided')
+		await ctx.message.add_reaction(emoji='❌')
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("?"),description='FridayCake',case_insensitive=True)
 bot.remove_command('help')
