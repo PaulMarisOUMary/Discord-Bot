38f553bdd3a6158c74e218db9602a9d9b55e4de7
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-01-18 10:19:30 +0100

    add support for command not found

--- alpha_server/cogs/basic.py
+++ alpha_server/cogs/basic.py
@@ -13,5 +13,9 @@ class Basic(commands.Cog, name="Basic"):
 		embed.set_footer(text="Requested by : "+str(ctx.message.author)+" à "+str(time.strftime('%H:%M:%S')), icon_url=ctx.message.author.avatar_url)
 		await ctx.send(embed=embed)
 
+	@commands.Cog.listener('on_command_error')
+	async def get_command_error(self, ctx, error):
+		await ctx.send(error)
+
 def setup(bot):
-    bot.add_cog(Basic(bot))
+	bot.add_cog(Basic(bot))
