96629d45ed2dac1b30ee2f8808adf92e53eca387
Paul Maris <paul.marisoumary@algosup.com>
2021-01-18 16:03:19 +0100

    add fridaycake cog

--- alpha_server/cogs/fridaycake.py
+++ alpha_server/cogs/fridaycake.py
@@ -2,7 +2,7 @@ import discord
 
 from discord.ext import commands
 
-class FridayCake(commands.Cog, name="admin", command_attrs=dict(hidden=True)):
+class FridayCake(commands.Cog, name="fridaycake", command_attrs=dict(hidden=True)):
 	def __init__(self, bot):
 		self.bot = bot
 
