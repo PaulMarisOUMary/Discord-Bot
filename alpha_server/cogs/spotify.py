28726937b2963cbf44d94a55bfd480240bf26356
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-02-02 00:04:16 +0100

    add detail on embed

--- alpha_server/cogs/spotify.py
+++ alpha_server/cogs/spotify.py
@@ -13,7 +13,8 @@ class Spotify(commands.Cog, name="spotify"):
 		if user == None: user = ctx.author
 		for activity in user.activities:
 			if str(activity) == "Spotify":
-				embed, keeper = discord.Embed(title="Spotify", colour=activity.colour, url="https://open.spotify.com/track/"+str(activity.track_id)), False
+				embed, keeper = discord.Embed(colour=activity.colour), False
+				embed.set_author(name="Spotify", url="https://open.spotify.com/track/"+str(activity.track_id), icon_url="https://toppng.com/uploads/thumbnail//spotify-logo-icon-transparent-icon-spotify-11553501653zkfre5mcur.png")
 				embed.add_field(name=activity.title, value=activity.artist, inline=False)
 				embed.set_thumbnail(url=activity.album_cover_url)
 				embed.set_footer(text=str(activity.duration)[2:-7]+" | Requested by : "+str(ctx.message.author.name)+" at "+str(time.strftime('%H:%M:%S')), icon_url=ctx.message.author.avatar_url)
