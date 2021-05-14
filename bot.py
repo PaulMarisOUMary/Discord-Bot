803a7c8619008660c515fd4e90843c7b9e140ff5
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-05-14 18:33:40 +0200

    handle path(s) on multiples OS

--- bot.py
+++ bot.py
@@ -7,11 +7,13 @@ intents = discord.Intents.default()
 intents.presences = True
 intents.members = True
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("?"), description='Algobot', intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), description='_Clan-of-Ghosts_', intents=intents)
 bot.remove_command('help')
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == '__main__':
-	for cog in os.listdir(os.getcwd()+"/cogs"):
+	for cog in os.listdir(current_directory+"\\cogs"):
 		if cog[-3:len(cog)] == '.py':
 			bot.load_extension('cogs.'+cog[:-3])
 
@@ -19,5 +21,5 @@ if __name__ == '__main__':
 async def on_ready():
 	print("Logged in as: "+str(bot.user)+"\nVersion: "+str(discord.__version__))
 
-token_file = open("auth/token.dat", "r").read()
+token_file = open(current_directory+"\\auth\\token.dat", "r").read()
 bot.run(token_file, bot=True, reconnect=True)
