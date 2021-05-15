9a6e7f13f03d384961922ff535a9c6a2bed2f50f
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-05-15 19:18:19 +0200

    update loop_task()

--- cogs/privatevocal.py
+++ cogs/privatevocal.py
@@ -19,7 +19,7 @@ class PrivateVocal(commands.Cog, name="privatevocal"):
 
 		def enumServers(servers=[]):
 			for guild in self.bot.guilds:
-				servers.append(guild)
+				if discord.utils.get(guild.channels, name=MAIN_CHANNEL): servers.append(guild)
 			return servers
 
 		def channelInfos(channels):
@@ -32,34 +32,32 @@ class PrivateVocal(commands.Cog, name="privatevocal"):
 
 		while not self.bot.is_closed():
 			for guild in enumServers():
-				channel = discord.utils.get(guild.channels, name=MAIN_CHANNEL)
-				if channel :
-					if not guild.id in data: data[guild.id] = [channel]
-					category = discord.utils.get(guild.categories, id=channel.category_id)
-					emptyChannels, usedChannels = channelInfos(data[guild.id])
-					is_change = False
+				if not guild.id in data: data[guild.id] = [channel]
+				category = discord.utils.get(guild.categories, id=channel.category_id)
+				emptyChannels, usedChannels = channelInfos(data[guild.id])
+				is_change = False
 
-					if usedChannels == len(data[guild.id]):
-						a = await guild.create_voice_channel(name="Vocal", category=category, sync_permissions=True)
-						data[guild.id].append(a)
-						is_change = True
+				if usedChannels == len(data[guild.id]):
+					a = await guild.create_voice_channel(name="Vocal", category=category, sync_permissions=True)
+					data[guild.id].append(a)
+					is_change = True
 
-					elif emptyChannels > 1:
-						count, lock = 0, False
-						for ch in data[guild.id]:
-							if not ch.members and not ch.name == MAIN_CHANNEL and not lock:
-								await ch.delete()
-								del data[guild.id][count]
-								lock = True
-							count += 1
-						is_change = True
+				elif emptyChannels > 1:
+					count, lock = 0, False
+					for ch in data[guild.id]:
+						if not ch.members and not ch.name == MAIN_CHANNEL and not lock:
+							await ch.delete()
+							del data[guild.id][count]
+							lock = True
+						count += 1
+					is_change = True
 
-					if is_change:
-						for i, ch in enumerate(data[guild.id]):
-							if not ch.name == MAIN_CHANNEL:
-								await ch.edit(name="Vocal "+str(i))
+				if is_change:
+					for i, ch in enumerate(data[guild.id]):
+						if not ch.name == MAIN_CHANNEL:
+							await ch.edit(name="Vocal "+str(i))
 
-			await asyncio.sleep(0.1)
+			await asyncio.sleep(0.5)
 
 def setup(bot):
 	bot.add_cog(PrivateVocal(bot))
