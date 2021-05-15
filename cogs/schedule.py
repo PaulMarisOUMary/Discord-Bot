11c149ace9a9d848bafaeaf52d019f2aad190a1c
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-05-16 00:08:36 +0200

    add variable day for nextday command

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -59,10 +59,10 @@ def task_in_img(final):
 class Schedule(commands.Cog, name="schedule"):
 	def __init__(self, bot):
 		self.bot = bot
-		end = os.getcwd()
-		CLIENT_ID = open(end+"/auth/client.dat", "r").read()
-		SECRET_ID = open(end+"/auth/secret.dat", "r").read()
-		TENANT_ID = open(end+"/auth/tenant_id.dat", "r").read()
+		source_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+		CLIENT_ID = open(os.path.join(source_directory, "auth", "client.dat"), "r").read()
+		SECRET_ID = open(os.path.join(source_directory, "auth", "secret.dat"), "r").read()
+		TENANT_ID = open(os.path.join(source_directory, "auth", "tenant_id.dat"), "r").read()
 		account = Account((CLIENT_ID, SECRET_ID), protocol=MSGraphProtocol(default_resource='planning@algosup.com'), auth_flow_type='credentials', tenant_id=TENANT_ID)
 		if account.authenticate(scope=['Calendars.Read.Shared', 'Calendars.Read']): pass#print('Authenticated!')
 		schedule = account.schedule()
@@ -90,7 +90,8 @@ class Schedule(commands.Cog, name="schedule"):
 		start = today
 		end = start + timedelta(1)
 		for event in await self.extract_calendar(start, end):
-			events.append(self.extract_infos(str(event)))
+			if not "start" in str(event):
+				events.append(self.extract_infos(str(event)))
 		image = task_in_img([events])
 
 		embed = discord.Embed(colour=0x474747)
@@ -100,12 +101,13 @@ class Schedule(commands.Cog, name="schedule"):
 		image.close()
 
 	@commands.command(name='nextcalendar', aliases=['nc'])
-	async def next_calendar(self, ctx):
+	async def next_calendar(self, ctx, more = 1):
 		today, events = datetime.now().date(), []
-		start = today + timedelta(1)
+		start = today + timedelta(more)
 		end = start + timedelta(1)
 		for event in await self.extract_calendar(start, end):
-			events.append(self.extract_infos(str(event)))
+			if not "start" in str(event):
+				events.append(self.extract_infos(str(event)))
 		image = task_in_img([events])
 
 		embed = discord.Embed(colour=0x474747)
@@ -120,7 +122,8 @@ class Schedule(commands.Cog, name="schedule"):
 		start = today - timedelta(days=today.weekday())
 		end = start + timedelta(days=5)
 		for event in await self.extract_calendar(start, end):
-			events.append(self.extract_infos(str(event)))
+			if not "start" in str(event):
+				events.append(self.extract_infos(str(event)))
 		for i in range(0, len(events)):
 			for j in range(0, len(events)-i-1):
 				if events[j][1] > events[j+1][1]:
@@ -131,6 +134,31 @@ class Schedule(commands.Cog, name="schedule"):
 				final.append(stock)
 				stock = []
 		
+		image = task_in_img(final)
+
+		embed = discord.Embed(colour=0x474747)
+		embed.set_image(url='attachment://schedule.png')
+		embed.set_footer(text="Requested by : "+str(ctx.message.author.name)+" à "+str(time.strftime('%H:%M:%S')), icon_url=ctx.message.author.avatar_url)
+		await ctx.send(file=discord.File(fp=image, filename='schedule.png'), embed=embed)
+		image.close()
+
+	@commands.command(name='nextweekcalendar', aliases=['nwc'])
+	async def next_week_calendar(self, ctx, more = 1):
+		today, events, stock, final = datetime.now().date(), [], [], []
+		start = today - timedelta(days=today.weekday()+(more*7))
+		end = start + timedelta(days=5)
+		for event in await self.extract_calendar(start, end):
+			if not "start" in str(event):
+				events.append(self.extract_infos(str(event)))
+		for i in range(0, len(events)):
+			for j in range(0, len(events)-i-1):
+				if events[j][1] > events[j+1][1]:
+					events[j], events[j+1] = events[j+1], events[j]
+		for i in range(0, len(events)):
+			stock.append(events[i])
+			if i == len(events)-1 or events[i][1] != events[i+1][1]:
+				final.append(stock)
+				stock = []
 		image = task_in_img(final)
 
 		embed = discord.Embed(colour=0x474747)
