f5964323c8e734a4d39e3251f0c4f1641c83d183
Paul Maris <paul.marisoumary@algosup.com>
2021-05-20 21:09:39 +0200

    extend search options, with aliases

--- cogs/basic.py
+++ cogs/basic.py
@@ -10,32 +10,40 @@ class Basic(commands.Cog, name="basic", command_attrs=dict(hidden=False)):
 
 	@commands.command(name='help', aliases=['?', 'h', 'commands'])
 	async def help(self, ctx, *input):
-		"""Show help command"""
+		"""Show help command, use : help {COMMAND/CATEGORY}"""
+		embed = discord.Embed(url='https://github.com/PaulMarisOUMary/Algosup-Discord')
+		remind = "\n**Remind** : Hooks such as {} must not be used when executing commands."
+		title, description, color = "Help · ", "", discord.Color.blue()
 		if not input:
-			embed = discord.Embed(title="Help · commands", url="https://github.com/PaulMarisOUMary/Algosup-Discord", description= "**Remind** : Hooks such as {} must not be used when executing commands.\nType `?help {CATEGORY}`", colour=discord.Color.blue())
 			category_list = ''
 			for cog in self.bot.cogs:
 				cog_settings = self.bot.get_cog(cog).__cog_settings__
-				if len(cog_settings) == 0: category_list += "{**"+str(cog).upper()+"**}\n *"+str(self.bot.cogs[cog].__doc__)+"*\n"
-				elif not cog_settings['hidden']: category_list += "{**"+str(cog).upper()+"**}\n *"+str(self.bot.cogs[cog].__doc__)+"*\n"
+				if len(cog_settings) == 0 or not cog_settings['hidden']: category_list += "{**"+str(cog).upper()+"**}\n *"+str(self.bot.cogs[cog].__doc__)+"*\n"
 			embed.add_field(name="Category :", value=category_list, inline=False)
 
 		elif len(input) == 1:
-			for cog in self.bot.cogs:
-				if cog.lower() == input[0].lower():
-					embed = discord.Embed(title="Help · " + str(cog), url="https://github.com/PaulMarisOUMary/Algosup-Discord", description="**Description** : "+str(self.bot.cogs[cog].__doc__)+"\n**Remind** : Hooks such as {} must not be used when executing commands.", color=discord.Color.green())
-					for command in self.bot.get_cog(cog).get_commands():
-						aliases = ''
-						if command.aliases: aliases = "__Aliases__ : "+str(command.aliases)+"\n"
-						embed.add_field(name="?"+str(command.name), value=aliases+str(command.help), inline=False)
-					break
+			search, search_command, search_cog = input[0].lower(), False, False
+			try:
+				search_command = self.bot.get_command(search)
+				search_cog = self.bot.cogs[search]
+			except: pass
 
-				else:
-					embed = discord.Embed(title="Help · Error", url="https://github.com/PaulMarisOUMary/Algosup-Discord", description="Category was not found", color=discord.Color.orange())
+			title = "Help · " + str(search)
+			if search_cog:
+				description, command_list = str(search_cog.__doc__), ''
+				for command in search_cog.get_commands():
+					command_list += "__"+str(command.name)+"__\n"+str(command.help)+'\n'
+				embed.add_field(name="Commands :", value=command_list, inline=False)
+			elif search_command:
+				description, color = '', discord.Color.green()
+				embed.add_field(name=str(search_command.name), value="__Aliases__ : `"+"`, `".join(search_command.aliases)+"`\n__Help__ : "+str(search_command.help), inline=False)
+			else:
+				title, description, color = "Help · Error", "Nothing was found", discord.Color.orange()
 
 		elif len(input) > 1:
-			embed = discord.Embed(title="Help · Error", url="https://github.com/PaulMarisOUMary/Algosup-Discord", description="Too many arguments", color=discord.Color.orange())
+			title, description, color = "Help · Error", "Too many arguments", discord.Color.orange()
 		
+		embed.title, embed.description, embed.color = title, remind + description, color
 		embed.set_footer(text="Requested by : "+str(ctx.message.author.name)+" at "+str(time.strftime('%H:%M:%S')), icon_url=ctx.message.author.avatar_url)
 		await ctx.send(embed=embed)
 
@@ -47,11 +55,11 @@ class Basic(commands.Cog, name="basic", command_attrs=dict(hidden=False)):
 		ping = (time.monotonic() - before) * 1000
 		await message.edit(content=f":ping_pong: Pong ! in `{float(round(ping/1000.0,3))}s` ||{int(ping)}ms||")
 
-	@commands.Cog.listener('on_command_error')
+	"""@commands.Cog.listener('on_command_error')
 	async def get_command_error(self, ctx, error):
 		if isinstance(error, commands.CommandNotFound):
 			await ctx.send(error)
-		else : await ctx.send("`C0DE 3RR0R` : " + error)
+		else : await ctx.send("`C0DE 3RR0R` : " + error)"""
 
 def setup(bot):
 	bot.add_cog(Basic(bot))
