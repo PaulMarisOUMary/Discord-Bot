6ee67a5264072b0a53d03e8a39a7f32f5a19c86e
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-04-30 20:22:19 +0200

    update SampleModal to CustomModal

--- views/modal.py
+++ views/modal.py
@@ -1,34 +1,64 @@
 import discord
+import functools
 
 from views.view import View as Parent
 
-class SampleModal(discord.ui.Modal, title="Sample Modal"):
-	name = discord.ui.TextInput(
-		label = "Name (required)",
-		placeholder = "Your name here...",
-		required = True,
-		min_length = 3
-	)
+class CustomModal(discord.ui.Modal):
+	def __init__(self, title: str, fields: dict[str, discord.ui.TextInput], when_submit: functools.partial):
+		super().__init__(title=title)
 
-	feedback = discord.ui.TextInput(
-		label = "What do you think of this new feature?",
-		placeholder = "Type your feedback here...",
-		style = discord.TextStyle.long,
-		required = False,
-		max_length = 300
-	)
+		self.values : dict[str, str] = {}
+		self.when_submit = when_submit
+
+		self.__fields : dict[str, functools.partial] = {}
+		for i, item in enumerate(fields.items()):
+			key, value = item
+			self.__fields[key] = functools.partial(self.__get_value, self.add_item(value).children[i])
+
+	def __get_value(self, children: discord.ui.TextInput) -> str:
+		return children.value
 
 	async def on_submit(self, interaction: discord.Interaction):
-		await interaction.response.send_message(f"Thanks for your feedback, `{self.name.value}` !\n{self.feedback.value}", ephemeral=True)
+		for key, value in self.__fields.items():
+			self.values[key] = value()
 
-	async def on_error(self, error: Exception, interaction: discord.Interaction, item: any) -> None:
-		await interaction.response.send_message("Oops! Something went wrong.", ephemeral=True)
-
+		await self.when_submit(self, interaction)
+		
 
 class View(Parent):
 	"""Button to Modal"""
 	def __init__(self, source, label, style=discord.ButtonStyle.grey, emoji=None, disabled=False):
 		super().__init__()
+
+		async def when_submit(_class: CustomModal, interaction: discord.Interaction):
+			formater = ''
+			for key, value in _class.values.items():
+				formater += f"\n`{key}`: `{value}`"
+
+			await interaction.response.send_message(f"You submitted: {formater}")
+
+		self.modal = CustomModal(
+			title = "Custom Modal",
+			fields = {
+				"name": discord.ui.TextInput(
+					label = "Name (required)",
+					placeholder = "Your name here...",
+					style = discord.TextStyle.short,
+					required = True,
+					min_length = 3
+				),
+				"feedback": discord.ui.TextInput(
+					label = "Feedback (optional)",
+					placeholder = "Your feedback here...",
+					style = discord.TextStyle.long,
+					required = False,
+					min_length = 5,
+					max_length = 300
+				)
+			},
+			when_submit = when_submit
+		)
+
 		self.source = source
 		self.button.label = label
 		self.button.style = style
@@ -39,7 +69,7 @@ class View(Parent):
 		if self.source.author != interaction.user:
 			await interaction.response.send_message("You can't open this modal.", ephemeral=True)
 		else:
-			await interaction.response.send_modal(SampleModal())
+			await interaction.response.send_modal(self.modal)
 
 	@discord.ui.button(style = discord.ButtonStyle.blurple)
 	async def button(self, interaction: discord.Interaction, button: discord.ui.Button):
