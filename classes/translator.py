84400c0215cd960f28d743630a4a75009af9ee47
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-05-02 11:20:22 +0200

    improve translation options for user
    - add `Translator.get_flag_abbv` method
    - improve `Translator.CORRECT_CONVERSION` to match the `discord.Locale` implementation
    - improve translation in the `cogs.contextmenu`
    - add `Translate` context command in `cogs.contextmenu`

--- classes/translator.py
+++ classes/translator.py
@@ -3,11 +3,20 @@ from googletrans import Translator as GT # pip install googletrans==4.0.0-rc1
 class Translator:
 
     CORRECT_CONVERSION: dict[str, str] = {
-        "en": "gb",
-        "ja": "jp",
-        "zh-CN": "cn",
-        "ko": "kr",
+        "cs": "cz",
+        "da": "dk",
         "el": "gr",
+        "en": "gb",
+        "en-GB": "gb",
+        "en-US": "us",
+        "es-ES": "es",
+        "hi": "in",
+        "ja": "jp",
+        "ko": "kr",
+        "pt-BR": "br",
+        "sv-SE": "sv",
+        "zh-CN": "cn",
+        "zh-TW": "tw",
     }
 
     @staticmethod
@@ -19,9 +28,14 @@ class Translator:
         return GT().translate(content, dest=dest, src=src).text
 
     @staticmethod
-    def get_emoji(code_lang: str) -> str:
+    def get_flag_abbr(code_lang: str) -> str:
         if code_lang in Translator.CORRECT_CONVERSION:
-            code_lang = Translator.CORRECT_CONVERSION[code_lang]
+            return Translator.CORRECT_CONVERSION[code_lang]
+        return code_lang
+
+    @staticmethod
+    def get_emoji(code_lang: str) -> str:
+        code_lang = Translator.get_flag_abbr(code_lang)
         
         RISLA = 0x1f1e6 # Regional Indicator Symbol Letter A
         LSLA = 0x61 # Latin Small Letter A
--- cogs/contextmenu.py
+++ cogs/contextmenu.py
@@ -1,4 +1,5 @@
 import discord
+import re
 
 from classes.translator import Translator
 
@@ -17,8 +18,13 @@ class ContextMenu(commands.Cog):
                 #guild_ids=[id, id, ...]
             ),
             app_commands.ContextMenu(
-                name = "Translate to English",
-                callback = self.translate,
+                name = "Translate in English",
+                callback = self.translate_to_english,
+                type = discord.AppCommandType.message,
+            ),
+            app_commands.ContextMenu(
+                name = "Translate",
+                callback = self.translate_to_your_language,
                 type = discord.AppCommandType.message,
             )
         ]
@@ -33,13 +39,30 @@ class ContextMenu(commands.Cog):
     async def join_date(self, interaction: discord.Interaction, member: discord.Member):
         await interaction.response.send_message(f"{member.mention} joined the {discord.utils.format_dt(member.joined_at)}", ephemeral=True)
 
-    async def translate(self, interaction: discord.Interaction, message: discord.Message):
+    async def translate(self, interaction: discord.Interaction, message: discord.Message, destination: str = "en"):
         content = message.content
+
+        mention_regex = re.compile(r"<[@|@& ]*&*[0-9]+>") 	#@
+        channel_regex = re.compile(r"<# [0-9]+>")			##
+        emote_regex = re.compile(r"<: \w+: [0-9]+>") 		#::
 
         analysis = Translator.detect(content)
         flag_emoji = Translator.get_emoji(analysis)
-        translation = Translator.translate(message.content, dest="en", src="auto")
-        await interaction.response.send_message(f"{flag_emoji} -> {Translator.get_emoji('en')} **:** {translation}", ephemeral=True)
+        translation = Translator.translate(message.content, dest=destination, src="auto")
+
+        for regex in [mention_regex, channel_regex, emote_regex]:
+            targets = regex.findall(translation)
+            for target in targets:
+                translation = translation.replace(target, target.replace(' ', ''))
+
+        await interaction.response.send_message(f"{flag_emoji} -> {Translator.get_emoji(destination)} **:** {translation}", ephemeral=True)
+
+    async def translate_to_english(self, interaction: discord.Interaction, message: discord.Message):
+        await self.translate(interaction, message, "en")
+
+    async def translate_to_your_language(self, interaction: discord.Interaction, message: discord.Message):
+        dest = Translator.get_flag_abbr(str(interaction.locale))
+        await self.translate(interaction, message, dest)
 
 async def setup(bot):
-	await bot.add_cog(ContextMenu(bot))
+    await bot.add_cog(ContextMenu(bot))
