c9b6c76efb063dfa149a7807bd7b80639e2f34b1
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-05-03 12:14:34 +0200

    Support more languages flag
    - improve `get_flag_abbr` method

--- classes/translator.py
+++ classes/translator.py
@@ -2,21 +2,120 @@ from googletrans import Translator as GT # pip install googletrans==4.0.0-rc1
 
 class Translator:
 
+    # doc: https://cloud.google.com/translate/docs/languages
     CORRECT_CONVERSION: dict[str, str] = {
+        "af": "za",
+        "am": "et",
+        "ar": "ae",
+        "az": "az", # key == value
+        "be": "by",
+        "bg": "bg", # key == value
+        "bn": "bd",
+        "bs": "ba",
+        "ca": "es", # invalid flag
+        "ceb": "ph",
+        "co": "fr", # invalid flag
         "cs": "cz",
+        "cy": "gb",
         "da": "dk",
+        "de": "de", # key == value
         "el": "gr",
         "en": "gb",
-        "en-GB": "gb",
-        "en-US": "us",
-        "es-ES": "es",
+        "en-gb": "gb",
+        "en-us": "us",
+        "eo": "pl",
+        "es": "es", # key == value
+        "es-es": "es",
+        "et": "ee",
+        "eu": "es",
+        "fa": "ir",
+        "fi": "fi", # key == value
+        "fr": "fr", # key == value
+        "fy": "nl", # invalid flag
+        "ga": "ie",
+        "gd": "en",
+        "gl": "es", # invalid flag
+        "gu": "in", # invalid flag
+        "ha": "ng",
+        "haw": "us", # invalid flag
+        "he": "il",
         "hi": "in",
+        "hmn": "la",
+        "hr": "hr", # key == value
+        "ht": "us", # invalid flag
+        "hu": "hu", # key == value
+        "hy": "am",
+        "id": "id", # key == value
+        "ig": "ng", # invalid flag
+        "is": "is", # key == value
+        "it": "it", # key == value
+        "iw": "il",
         "ja": "jp",
+        "jw": "id",
+        "ka": "ge",
+        "kk": "kz",
+        "km": "kh",
+        "kn": "in",
         "ko": "kr",
-        "pt-BR": "br",
-        "sv-SE": "sv",
-        "zh-CN": "cn",
-        "zh-TW": "tw",
+        "ku": "iq",
+        "ky": "kg",
+        "la": None, # ! critical invalid flag
+        "lb": "lu",
+        "lo": "la",
+        "lt": "lt", # key == value
+        "lv": "lv", # key == value
+        "mg": "mg", # key == value
+        "mi": "nz",
+        "mk": "mk", # key == value
+        "ml": "in", # invalid flag
+        "mn": "mn", # key == value
+        "mr": "in",
+        "ms": "in",
+        "mt": "mt", # key == value
+        "my": "mm",
+        "ne": "np",
+        "nl": "nl", # key == value
+        "no": "no", # key == value
+        "ny": "mw",
+        "or": "in",
+        "pa": "in", # invalid flag
+        "pl": "pl", # key == value
+        "ps": "af",
+        "pt": "pt", # key == value
+        "pt-br": "br",
+        "ro": "ro", # key == value
+        "ru": "ru", # key == value
+        "sd": "pk",
+        "si": "lk",
+        "sk": "sk", # key == value
+        "sl": "si",
+        "sm": "ws",
+        "sn": "zw",
+        "so": "so", # key == value
+        "sq": "al",
+        "sr": "rs",
+        "st": "ls",
+        "su": "sd",
+        "sv": "se",
+        "sv-se": "sv",
+        "sw": "ke",
+        "ta": "in", # invalid flag
+        "te": "in", # invalid flag
+        "tg": "tj",
+        "th": "th", # key == value
+        "tl": "hn",
+        "tr": "tr", # key == value
+        "ug": "cn",
+        "uk": "uk", # key == value
+        "ur": "pk",
+        "uz": "uz", # key == value
+        "vi": "vn",
+        "xh": "za",
+        "yi": "il", # invalid flag
+        "yo": None, # ! critical invalid flag
+        "zh-cn": "cn",
+        "zh-tw": "tw",
+        "zu": "za" # invalid flag
     }
 
     @staticmethod
@@ -29,9 +128,7 @@ class Translator:
 
     @staticmethod
     def get_flag_abbr(code_lang: str) -> str:
-        if code_lang in Translator.CORRECT_CONVERSION:
-            return Translator.CORRECT_CONVERSION[code_lang]
-        return code_lang
+        return Translator.CORRECT_CONVERSION.get(code_lang.lower(), code_lang.lower())
 
     @staticmethod
     def get_emoji(code_lang: str) -> str:
