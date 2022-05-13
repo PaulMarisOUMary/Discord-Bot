883e5665afa91b4041cb4273c9f1fc356122f7b7
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-05-13 07:38:08 +0200

    starting to refactor database.py
    - new `insert_onduplicate` method
    - new private methods for convenience

--- classes/database.py
+++ classes/database.py
@@ -1,6 +1,7 @@
 import aiomysql
 import asyncio
 
+from typing import Union
 from datetime import datetime, date
 
 class DataSQL():
@@ -35,7 +36,82 @@ class DataSQL():
 
                 except Exception as e:
                     raise e
+
+    def __toKindFormat(self, value: any = None) -> any:
+        keeper = value
+        if isinstance(value, (str)): value = f"'{value}'"
+        elif isinstance(value, date): value = f"'{value.strftime('%Y-%m-%d')}'"
+        elif isinstance(value, datetime): value = f"'{value.strftime('%Y-%m-%d %H:%M:%S')}'"
+
+        if value != keeper: return value
+
+    def __to_insert_variables_values(self, dictionnary: dict) -> tuple[str, str]:
+        variables, values = '', ''
+        for i, items in enumerate(dictionnary.items()):
+            variable, value = items[0], items[1]
+            
+            variables += f"`{variable}`, " if i+1 < len(dictionnary) else f"`{variable}`"
+
+            kindFormat = self.__toKindFormat(value)
+            if kindFormat: value = kindFormat
+            
+            values += f"{value}, " if i+1 < len(dictionnary) else f"{value}"    
+
+        return variables, values
+
+    def __to_update_variables_values(self, dictionnary: dict) -> str:
+        assignment = ''
+        for i, items in enumerate(dictionnary.items()):
+            variable, value = items[0], items[1]
+
+            kindFormat = self.__toKindFormat(value)
+            if kindFormat: value = kindFormat
+            
+            assignment += f"`{variable}`={value}, " if i+1 < len(dictionnary) else f"`{variable}`={value}"    
+
+        return assignment
+
+    def __query_insert(self, table: str, dict: dict, close: bool = True) -> str:
+        query = f"INSERT INTO `{table}` ("
+
+        variables, values = self.__to_insert_variables_values(dict)
+        query += f"{variables} ) VALUES ({values})"	 
+
+        if close: query += ';'
+        return query
     
+    def __query_update(self, table: str, dict: dict, condition: Union[str, None] = None, close: bool = True) -> str:
+        query = f"UPDATE `{table}` SET "
+
+        assignement = self.__to_update_variables_values(dict)
+        query += assignement
+
+        if condition: query += f" WHERE {condition}"
+
+        if close: query += ';'
+        return query
+
+    async def insert(self, table: str, dict: dict) -> query:
+        query = self.__query_insert(table, dict)
+        return await self.query(query)
+
+    async def insert_onduplicate(self, table: str, insert_dict: dict, update_dict: Union[dict, None] = None) -> query:
+        if not update_dict: update_dict = insert_dict
+
+        insert_query = self.__query_insert(table, insert_dict, close=False)
+        assignment = self.__to_update_variables_values(update_dict)
+
+        query = f"{insert_query} ON DUPLICATE KEY UPDATE {assignment};"
+
+        return await self.query(query)
+
+    async def update(self, table: str, dict: str, condition: str = None) -> query: # !need-refactor
+        query = self.__query_update(table, dict, condition)
+        return await self.query(query)
+
+    async def increment(self, table: str, target: str, value: int = 1, condition: Union[str, None] = None) -> update: # !need-refactor
+        await self.update(table, target, f"{target} + {value}", condition, True)
+
     async def select(self, table: str, target: str, condition: str = '', order: str = '', limit: str = '') -> query:
         query = f"SELECT {target} FROM `{table}`"
         if condition: query += f" WHERE {condition}"
@@ -52,42 +128,6 @@ class DataSQL():
     async def exist(self, table: str, target: str, condition: str = '') -> bool:
         response = await self.count(table, target, condition)
         return response[0][0] > 0
-
-    async def insert(self, table: str, args: dict) -> query:
-        query, variables, values, lenght = f"INSERT INTO `{table}` (", '', '', len(args)
-        for i, items in enumerate(args.items()):
-            variable, value = items[0], items[1]
-            
-            variables += f"`{variable}`, " if i+1 < lenght else f"`{variable}`"
-
-            kindFormat = self.__toKindFormat(value)
-            if kindFormat: value = kindFormat
-            
-            values += f"{value}, " if i+1 < lenght else f"{value}"    
-
-        query += f"{variables} ) VALUES ({values});"	 
-
-        return await self.query(query)
-
-    async def update(self, table: str, variable: str, value: any, condition: str = None, mixedtypes: bool = False) -> query:
-        if not mixedtypes:
-            kindFormat = self.__toKindFormat(value)
-            if kindFormat: value = kindFormat
-
-        query = f"UPDATE {table} SET `{variable}` = {value}"
-        if condition: query += f" WHERE {condition}"
-        return await self.query(query + ';')
-
-    async def increment(self, table: str, target: str, value: int = 1, condition: str = None) -> update:
-        await self.update(table, target, f"{target} + {value}", condition, True)
-
-    def __toKindFormat(self, value: any = None) -> any:
-        keeper = value
-        if isinstance(value, (str)): value = f"'{value}'"
-        elif isinstance(value, date): value = f"'{value.strftime('%Y-%m-%d')}'"
-        elif isinstance(value, datetime): value = f"'{value.strftime('%Y-%m-%d %H:%M:%S')}'"
-
-        if value != keeper: return value
 
     async def close(self) -> None:
         self.pool.close()
