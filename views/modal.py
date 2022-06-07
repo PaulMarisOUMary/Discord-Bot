7838070b573a1251de17b2393efbac1c0bb0c9b5
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-06-07 16:54:51 +0200

    add discord.ui.Select support in CustomModal
    & bump to `ae51d5849a946699362372fb9d7f637bb81186f3` of discord.py

--- views/modal.py
+++ views/modal.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from views.view import View as Parent
 
 class CustomModal(discord.ui.Modal):
-	def __init__(self, title: str, fields: dict[str, discord.ui.TextInput], when_submit: functools.partial):
+	def __init__(self, title: str, fields: dict[str, Union[discord.ui.TextInput, discord.ui.Select]], when_submit: functools.partial):
 		super().__init__(title=title)
 
 		self.values : dict[str, str] = {}
@@ -17,8 +17,13 @@ class CustomModal(discord.ui.Modal):
 			key, value = item
 			self.__fields[key] = functools.partial(self.__get_value, self.add_item(value).children[i])
 
-	def __get_value(self, children: discord.ui.TextInput) -> str:
-		return children.value
+	def __get_value(self, children: Union[discord.ui.TextInput, discord.ui.Select]) -> str:
+		try:
+			return children.value
+		except AttributeError:
+			return children.values
+		except Exception as e:
+			raise e
 
 	async def on_submit(self, interaction: discord.Interaction):
 		for key, value in self.__fields.items():
@@ -39,9 +44,12 @@ class View(Parent):
 		async def when_submit(_class: CustomModal, interaction: discord.Interaction):
 			formater = ''
 			for key, value in _class.values.items():
-				formater += f"\n`{key}`: `{value}`"
+				if isinstance(value, list):
+					formater += f"\n{key}: {', '.join(value)}"
+				else:
+					formater += f"\n{key}: {value}"
 
-			await interaction.response.send_message(f"You submitted: {formater}")
+			await interaction.response.send_message(f"__You submitted__ **:** {formater}")
 
 		self.modal = CustomModal(
 			title = "Custom Modal",
@@ -60,6 +68,19 @@ class View(Parent):
 					required = False,
 					min_length = 5,
 					max_length = 300
+				),
+				"mood": discord.ui.Select(
+					placeholder="Mood (required)",
+					min_values=1,
+					max_values=5,
+					options=[
+						discord.SelectOption(label="Happy", value='😁', emoji='😁'),
+						discord.SelectOption(label="Good", value='😊', emoji='😊'),
+						discord.SelectOption(label="Neutral", value='😐', emoji='😐'),
+						discord.SelectOption(label="Sad", value='😢', emoji='😢'),
+						discord.SelectOption(label="Angry", value='😡', emoji='😡'),
+					],
+					disabled=False # Must be False, else the user will not be able to send the modal
 				)
 			},
 			when_submit = when_submit
