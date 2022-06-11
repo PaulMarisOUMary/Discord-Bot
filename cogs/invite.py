c9de45d94c158d80ad1ee571d87ccf107d9664c7
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2022-06-11 16:08:34 +0200

    add return on lenght_message > 4096

--- cogs/invite.py
+++ cogs/invite.py
@@ -169,6 +169,9 @@ class Invite(commands.Cog, name="invite"):
             await self.bot.database.update(self.subconfig_data["table"], {"custom_message": MixedTypes("NULL")}, f"guild_id = {ctx.guild.id}")
             await ctx.send(f"Logs message set to default.")
             return
+        elif lenght_message := len(str(message)) >= 4096:
+            await ctx.send(f"Logs message is too long. (Should be less than 4096 characters and is {lenght_message})")
+            return
 
         class FakeInvite():
             approximate_member_count = 111
