5713ea8af871c3ee64fa7dcfdae1eeaaa628f00c
Paul MarisOUMary <paul.marisoumary@gmail.com>
2023-08-11 15:11:38 +0200

    Change to CustomActivity and fix reconnect issue using a task

--- cogs/status.py
+++ cogs/status.py
@@ -1,7 +1,6 @@
-import asyncio
 import discord
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 from classes.discordbot import DiscordBot
 
@@ -9,6 +8,9 @@ class Status(commands.Cog, name="status"):
 	"""A loop to set the current status of the bot."""
 	def __init__(self, bot: DiscordBot) -> None:
 		self.bot = bot
+		self.subconfig_data = self.bot.config["cogs"][self.__cog_name__.lower()]
+
+		self.count = 0
 
 	"""def help_custom(self) -> tuple[str]:
 		emoji = '🏷️'
@@ -17,23 +19,25 @@ class Status(commands.Cog, name="status"):
 		return emoji, label, description"""
 
 	async def cog_load(self) -> None:
-		self.task_change_status = self.bot.loop.create_task(self.loop_change_status())
+		self.task_change_status.change_interval(seconds=self.subconfig_data["cooldown"])
+		self.task_change_status.start()
 
 	async def cog_unload(self) -> None:
 		self.task_change_status.cancel()
 
-	async def loop_change_status(self) -> None:
+	@tasks.loop()
+	async def task_change_status(self) -> None:
 		await self.bot.wait_until_ready()
-		subconfig_data = self.bot.config["cogs"][self.__cog_name__.lower()]
-		while not self.bot.is_closed():
-			for status in subconfig_data["status"]:
-				await self.bot.change_presence(
-					activity=discord.Streaming(
-						name=status, 
-						url="https://www.twitch.tv/warriormachine_"), 
-					status=discord.Status.do_not_disturb
-				)
-				await asyncio.sleep(subconfig_data["cooldown"])
+
+		await self.bot.change_presence(
+			activity=discord.CustomActivity(
+				name=self.subconfig_data["status"][self.count],
+				emoji=None # Not supported (yet) by discord
+			), 
+			status=discord.Status.do_not_disturb
+		)
+
+		self.count = (self.count + 1) % len(self.subconfig_data["status"])
 
 
 
