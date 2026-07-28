98a2f7d4c99a6ab760ab66e84455e4b3798d5eb6
PaulMarisOUMary <paul.marisoumary@gmail.com>
2026-07-28 16:34:07 +0200

    Update basic

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,40 +1,44 @@
 import time
 
+from discord import Color, Embed
 from discord.ext import commands
 
-from utils.basebot import DiscordBot
-from utils.helper import bot_has_permissions
+from utils.bot import DiscordBot
 
 
 class Basic(commands.Cog, name="basic"):
-	"""
-		Basic commands, like ping.
-		
-		Require intents: 
-			- None
-		
-		Require bot permission:
-			- send_messages
-	"""
-	def __init__(self, bot: DiscordBot) -> None:
-		self.bot = bot
+    """
+    Basic commands, like ping.
 
-	def help_custom(self) -> tuple[str, str, str]:
-		emoji = '📙'
-		label = "Basic"
-		description = "Basic commands, like ping."
-		return emoji, label, description
+    Require intents:
+        - None
 
-	@bot_has_permissions(send_messages=True)
-	@commands.hybrid_command(name="ping", description="Ping the bot.")
-	@commands.cooldown(1, 5, commands.BucketType.user)
-	async def ping(self, ctx: commands.Context) -> None:
-		"""Show latency in seconds & milliseconds"""
-		before = time.monotonic()
-		message = await ctx.send(":ping_pong: Pong !")
-		ping = (time.monotonic() - before) * 1000
-		await message.edit(content=f":ping_pong: Pong ! in `{float(round(ping/1000.0,3))}s` ||{int(ping)}ms||")
+    Require bot permission:
+        - send_messages
+    """
+
+    def __init__(self, bot: DiscordBot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(name="ping", description="Ping the bot.")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def ping(self, ctx: commands.Context) -> None:
+        ws_latency = round(self.bot.latency * 1000)
+
+        text = ":ping_pong: Pong !"
+
+        start_time = time.perf_counter()
+        message = await ctx.send(text)
+        end_time = time.perf_counter()
+
+        api_latency = round((end_time - start_time) * 1000)
+
+        embed = Embed(title=text, color=Color.green())
+        embed.add_field(name="WebSocket", value=f"`{ws_latency} ms`", inline=True)
+        embed.add_field(name="API Round-Trip", value=f"`{api_latency} ms`", inline=True)
+
+        await message.edit(content=None, embed=embed)
 
 
 async def setup(bot: DiscordBot) -> None:
-	await bot.add_cog(Basic(bot))
+    await bot.add_cog(Basic(bot))
