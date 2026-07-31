b315dda22077c2134b96c5e149811dcfc560e839
PaulMarisOUMary <paul.marisoumary@gmail.com>
2026-07-31 16:30:49 +0200

    Add upsert & increment

--- utils/database/crud.py
+++ utils/database/crud.py
@@ -15,3 +15,39 @@ def get_table(model: type[ModelType]) -> Table:
         raise TypeError(f"{model!r} is not a mapped SQLModel table")
 
     return mapper.local_table
+
+
+async def upsert(
+    session: AsyncSession, obj: ModelType, *, exclude: Iterable[str] = ()
+) -> None:
+    table = get_table(type(obj))
+    pk_columns = {column.name for column in table.primary_key.columns}
+    skip = pk_columns | set(exclude)
+
+    values = obj.model_dump()
+    update = {key: value for key, value in values.items() if key not in skip}
+
+    statement = mysql_insert(table).values(**values)
+    if update:
+        statement = statement.on_duplicate_key_update(**update)
+
+    await session.exec(statement)
+
+
+async def increment(
+    session: AsyncSession,
+    model: type[ModelType],
+    keys: dict[str, Any],
+    target: str,
+    *,
+    amount: int = 1,
+) -> None:
+    table = get_table(model)
+    column = table.c[target]
+
+    statement = mysql_insert(table).values(**keys, **{target: amount})
+    statement = statement.on_duplicate_key_update(
+        **{target: func.coalesce(column, 0) + amount}
+    )
+
+    await session.exec(statement)
